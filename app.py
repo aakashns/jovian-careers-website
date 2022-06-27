dd7fafefa559682380ddfd6fe80c10e026649fb9
Aakash N S <aakashns@jovian.ai>
2022-06-27 16:04:52 +0000

    added HTML and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, Jovian!"
+def hello_jovian():
+    return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
