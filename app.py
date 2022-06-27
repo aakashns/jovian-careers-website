0fab54fbf86d7e3d1c23971b6802ae2b0e18cddf
Aakash N S <aakashns@jovian.ai>
2022-06-27 16:48:02 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  }
+]
+
 @app.route("/")
 def hello_jovian():
-    return render_template('home.html')
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
